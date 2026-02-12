c355457d038bed01e95894479242be3655392de1
Mikalai <mb@3nsoft.com>
2026-02-11 22:50:16 -0500

    NAPI plugin with NaCl cryptors for 3NWeb client library and desktop

---
+++ README.md
@@ -0,0 +1,3 @@
+# NAPI NaCl plugin
+
+
---
+++ build-all-cross-compiled.sh
@@ -0,0 +1,7 @@
+for target in $(cat package.json | jq -r .napi.targets[])
+do
+  echo "-----------------------------------------------"
+  echo "|  compiling for $target"
+  echo "-----------------------------------------------"
+  npm run build -- --cross-compile --target $target
+done
